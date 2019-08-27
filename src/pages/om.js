0109904ceea6a56445e73c22be976c906d1fcb0d
Erik Löfblad <erikhinnerdal@gmail.com>
2019-08-27 23:53:29 +0200

    SEO

--- src/pages/om.js
+++ src/pages/om.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const AboutPage = () => {
   return (
     <Layout>
+      <SEO title="Om Radileaf"></SEO>
       <h1>Om Radileaf</h1>
     </Layout>
   )
--- src/pages/produkter.js
+++ src/pages/produkter.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const ProductsPage = () => {
   return (
     <Layout>
+      <SEO title="produkter" />
       <h1>Produkter</h1>
     </Layout>
   )
